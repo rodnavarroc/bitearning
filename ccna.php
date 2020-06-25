44eaeeacbf8310607b06112bd75ffdb9012a4a43
rodnavarroc <rodolfo.0926@hotmail.com>
2020-06-25 00:03:40 -0500

    Commit inicial

---
+++ ccna.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bit Learning</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="https://kit.fontawesome.com/94f47f52fa.js" crossorigin="anonymous"></script>
+</head>
+<body style="font-family: Open Sans;">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger" style="font-family: Raleway;">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Bit Learning &nbsp;</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php#inicio"><i class="fas fa-home"></i>&nbsp;Inicio</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php#about"><i class="far fa-question-circle"></i>&nbsp;¿Quienes somos?</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php#contacto"><i class="fas fa-phone-alt"></i>&nbsp;Contacto</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="ccna.php"><i class="fas fa-server"></i>&nbsp;Curso CCNA</a>
+          </li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="navbarx d-block d-sm-none p-2" id="myNavbar">
+  <a class="nav-link" href="http://wa.me/528180830218" target="blank" style="border-radius: 99px; background-color: #1ad66f; color: white; float: right; font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+  </div>
+
+  <!-- Page Content -->
+  <div class="container" style="padding-top: 100px;">
+  	<div class="row">
+  		<div class="col-12">
+  			<center><h5 class="text-light bg-dark p-3">Curso CCNA 200-125 R&S</h5></center>
+		  	<center><h6 class="text-dark bg-light p-3">Curso de preparación para la certificación de Cisco CCNA 200-125 R&S</h6></center>
+  		</div>
+  		<div class="col-12 col-md-6 p-5">
+		  		<label><p>Temario:</p></label><br>
+			  	<p><i class="fas fa-check"></i>&nbsp;Network Fundamentals</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;LAN Switching Technologies</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;Routing Technologies</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;WAN Technologies</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;Infrastructure Services</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;Infrastructure Security</p>
+			  	<p><i class="fas fa-check"></i>&nbsp;Infrastructure Management</p>
+
+		  	<br><label><i class="far fa-clock"></i>&nbsp;Duración: <br>16 sesiones (64 horas)</label>
+		  	<br><label><i class="fas fa-brain"></i>&nbsp;Concimiento previo requerido: <br>Ninguno</label>
+
+		  	<br><br><center><a href="" class="btn btn-outline-primary">Quiero inscribirme</a></center>
+  		</div>
+  		<div class="col-12 col-md-6 p-5">
+  			<center><p><b>Modalidades del curso<hr></b></p></center>
+  			<p><b>Presencial</b></p>
+  			<div class="gmap_canvas"><iframe width="100%;" height="150px;" id="gmap_canvas" src="https://maps.google.com/maps?q=paseo%20de%20los%20leones%20123&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:200px;width:350px;}.gmap_canvas {overflow:hidden;background:none}</style>
+  			<br><p><b><i class="fas fa-stopwatch"></i>&nbsp;Horario:</b> Sábados de 9:00 AM a 1:00 PM</p>
+  			<p><b><i class="fas fa-flag-checkered"></i>&nbsp;Inicio del curso:</b> 21 de Septiembre</p>
+  			<p class="text-danger">Cupo limitado</p>
+
+  			<br><p>Online</p><hr>
+  			<p><b><i class="fas fa-stopwatch"></i>&nbsp;Horario:</b> Domingos de 2:00 PM a 6:00 PM</p>
+  			<p><b><i class="fas fa-flag-checkered"></i>&nbsp;Inicio del curso:</b> 22 de Septiembre</p>
+  			<p class="text-danger">Cupo limitado</p>
+
+  		</div>
+  	</div>
+  </div>
+
+  <br><br>
+  <!-- /.container -->
+  <div class="container-fluid my-auto">
+    <div class="row bg-dark text-light p-1">
+      <div class="col-12">
+        <center><p style="font-size: 12px;" class="my-auto">BitLearning | Todos los derechos reservados © 2020</p></center>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+<style type="text/css">
+  html {
+  scroll-behavior: smooth;
+}
+
+.navbarx {
+  background-color: transparent;
+  z-index: 99;
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+/* Style the links inside the navigation bar */
+.navbarx a {
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 3% 5%;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+body {
+  background-image: url("https://i.imgur.com/TR1mM8w.png");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+</html>
---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bit Learning</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="https://kit.fontawesome.com/94f47f52fa.js" crossorigin="anonymous"></script>
+</head>
+<body style="font-family: Open Sans;">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-danger" style="font-family: Raleway;">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Bit Learning &nbsp;</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#inicio"><i class="fas fa-home"></i>&nbsp;Inicio</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#about"><i class="far fa-question-circle"></i>&nbsp;¿Quienes somos?</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#contacto"><i class="fas fa-phone-alt"></i>&nbsp;Contacto</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="ccna.php"><i class="fas fa-server"></i>&nbsp;Curso CCNA</a>
+          </li>
+        </ul>
+    </div>
+  </nav>
+
+  <div class="navbarx d-block d-sm-none p-2" id="myNavbar">
+  <a class="nav-link" href="http://wa.me/528180830218" target="blank" style="border-radius: 99px; background-color: #1ad66f; color: white; float: right; font-size: 20px;"><i class="fab fa-whatsapp"></i></a>
+  </div>
+
+  <!-- Page Content -->
+  <div class="container" style="padding-top: 100px;" id="inicio">
+    <div class="row">
+      <div class="col-12" style="padding-top: 12%;">
+        <center><h3>¡Bienvenido/a!</h3></center>
+        <center><h5>En BitLearning te capacitamos para aumentar tu valor curricular</h5></center><br><br>
+        <center><h4>¿Por qué elegirnos?</h4></center>
+        <br>
+      </div>
+    </div>
+    <div class="row p-3 pt-5">
+      <div class="col-4">
+        <center>
+          <i class="fas fa-book" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Plan Educativo basado en CISCO</label>
+        </center>
+      </div>
+      <div class="col-4">
+        <center>
+          <i class="fas fa-user-check" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Instructores Certificados</label>
+        </center>
+      </div>
+      <div class="col-4">
+        <center>
+          <i class="fas fa-money-check-alt" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Facilidades de pago</label>
+        </center>
+      </div>
+    </div>
+    <div class="row p-3 pt-5">
+      <div class="col-4">
+        <center>
+          <i class="fas fa-briefcase" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Bolsa de Trabajo para alumnos</label>
+        </center>
+      </div>
+      <div class="col-4">
+        <center>
+          <i class="fas fa-map-marker-alt" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Ubicación céntrica</label>
+        </center>
+      </div>
+      <div class="col-4">
+        <center>
+          <i class="fas fa-server" style="font-size: 48px; color: #CFB53B;"></i><br><br>
+          <label>Equipo para practicar</label>
+        </center>
+      </div>
+    </div>
+    <br><br>
+    <div class="d-none d-sm-block"><center><a href="#about" class="btn btn-outline-dark btn-lg">Conoce más</a></center></div>
+  </div>
+
+  <div style="padding-top: 13%;"></div>
+  <div id="about" class="container-fluid text-light p-5" style="background-color: #58a7e0;">
+    <div class="row">
+      <h3 style="font-family: Raleway;">¿Quiénes somos?</h3>
+      <div class="col-12 col-md-8 mx-auto">
+        <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-light p-5" style="background-color: #2b2a2a;">
+    <div class="row">
+      <h3 style="font-family: Raleway;">¿Cómo son <br>nuestras clases?</h3>
+      <div class="col-12 col-md-8 mx-auto">
+        <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid text-light p-5" style="background-color: #bf2837;">
+    <div class="row">
+      <h3 style="font-family: Raleway;">¿A que estamos<br>comprometidos?</h3>
+      <div class="col-12 col-md-8 mx-auto">
+        <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+    </div>
+  </div>
+  <div class="container text-dark p-5" id="contacto">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <h3 style="font-family: Raleway;">Ponte en contacto</h3><br>
+        <p style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. <br><br>Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. <br><br>Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <hr>
+        <label><i class="fas fa-at"></i>&nbsp;<b>Correo electrónico: </b>info@bitlearning.com.mx</label><br>
+        <label><i class="fab fa-facebook-f"></i>&nbsp;<b>Facebook: </b>BitLearning</label><br>
+        <label><i class="fab fa-whatsapp"></i>&nbsp;<b>Whatsapp: </b>81 8083 0218</label><br><br>
+
+      </div>
+      <div class="col-12 col-md-6">
+        <form>
+          <div class="form-group">
+            <div class="d-block d-sm-none"><br></div>
+            <center><h5>¿Tienes preguntas?</h5></center><hr>
+            <label>Nombre:</label>
+            <input type="text" placeholder="Juan Pérez Martínez" class="form-control" required>
+            <br><label>Correo electrónico:</label>
+            <input type="email" placeholder="alguien@ejemplo.com" class="form-control" required>
+            <br><label>Escribe tu mensaje:</label>
+            <textarea class="form-control" rows="5" required></textarea>
+            <br><center><button type="submit" class="btn btn-outline-dark">Enviar</button></center>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="d-block d-sm-none"><br></div>
+        <h5>Visítanos</h5><hr><br>
+        <div class="gmap_canvas"><iframe width="100%;" height="350px;" id="gmap_canvas" src="https://maps.google.com/maps?q=paseo%20de%20los%20leones%20123&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div><style>.mapouter{position:relative;text-align:right;height:200px;width:350px;}.gmap_canvas {overflow:hidden;background:none}</style>
+      </div>
+    </div>
+  </div>
+  <!-- /.container -->
+
+  <div class="container-fluid my-auto">
+    <div class="row bg-dark text-light p-1">
+      <div class="col-12">
+        <center><p style="font-size: 12px;" class="my-auto">BitLearning | Todos los derechos reservados © 2020</p></center>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+<style type="text/css">
+  html {
+  scroll-behavior: smooth;
+}
+
+.navbarx {
+  background-color: transparent;
+  z-index: 99;
+  overflow: hidden;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+
+/* Style the links inside the navigation bar */
+.navbarx a {
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 3% 5%;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+body {
+  background-image: url("https://i.imgur.com/TR1mM8w.png");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+</html>
